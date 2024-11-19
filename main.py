b90fb5b1a2031ffd4911b471d5ab1992718bae5e
chuck <chuck.lindblom@jax.org>
2024-11-19 12:10:38 -0500

    player movement is working

--- main.py
+++ main.py
@@ -3,15 +3,23 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init() # Start pygame
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0), rect=None)
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
