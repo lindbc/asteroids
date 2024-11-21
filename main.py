da718c4254647dae909fad8d84747d882ba6d2dc
chuck <chuck.lindblom@jax.org>
2024-11-21 10:36:35 -0500

    asteroids working

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
+# Notes
+# python3 -m venv venv - create virtual environment
+# source venv/bin/activate - move to virtual environment
+
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
+    # Start pygame
     pygame.init() # Start pygame
+    
+    # Create the clock that controls the game
     clock = pygame.time.Clock()
     dt = 0
+
+    # Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Create Player
+    Player.containers = (updatable, drawable) # add to containers before creating the player to make sure they are in it for code
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
+    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0), rect=None)
-        player.update(dt)
-        player.draw(screen)
+        for thing in updatable:
+            thing.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
